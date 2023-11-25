d1756495ae100a72fb5e398042420ec561536bea
jckang <jckang568@gmail.com>
2023-11-25 22:35:32 +0900

    #chore: "패키지 구조 변경 및 의존성 변경"
    - 멀티 모듈 세팅
    - 1. 소스 위치 변경
    - 2. gradle 의존성 설정 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,41 +1,60 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.1.6"
-    id("io.spring.dependency-management") version "1.1.4"
     kotlin("jvm") version "1.8.22"
-    kotlin("plugin.spring") version "1.8.22"
+    kotlin("plugin.spring") version "1.8.22" apply false
+    id("org.springframework.boot") version "3.1.6" apply false
+    id("io.spring.dependency-management") version "1.1.4" apply false
 }
 
-group = "dev.jayce"
-version = "0.0.1-SNAPSHOT"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
 }
 
-repositories {
-    mavenCentral()
-}
 
-dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs += "-Xjsr305=strict"
-        jvmTarget = "17"
+allprojects {
+    group = "dev.jayce"
+    version = "0.0.1-SNAPSHOT"
+    repositories {
+        mavenCentral()
     }
 }
 
-tasks.withType<Test> {
-    useJUnitPlatform()
+subprojects {
+    apply (plugin = "org.jetbrains.kotlin.jvm")
+    apply (plugin = "org.jetbrains.kotlin.plugin.spring")
+    apply (plugin = "org.springframework.boot")
+    apply (plugin = "io.spring.dependency-management")
+    dependencies {
+        // implementation("org.springframework.boot:spring-boot-starter-web")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+    }
+
+    tasks.getByName("bootJar") {
+        enabled = false
+    }
+
+    tasks.getByName("jar") {
+        enabled = false
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs += "-Xjsr305=strict"
+            jvmTarget = "17"
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
+    /*tasks.bootBuildImage {
+        builder.set("paketobuildpacks/builder-jammy-base:latest")
+    }*/
 }
 
-tasks.bootBuildImage {
-    builder.set("paketobuildpacks/builder-jammy-base:latest")
-}
+
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,5 @@
 rootProject.name = "practice-multi-module"
+
+include (
+    "practice-multi-module-api"
+)
